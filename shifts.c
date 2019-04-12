0ea5ba44e6320f83a04a97a9df44a0005308d832
Aritra Sarkar <aritrasrkr1911@gmail.com>
2019-04-12 22:18:02 +0530

    Implement left shift

--- shifts.c
+++ shifts.c
@@ -1,6 +1,8 @@
 #include <ctype.h>
 #include "shifts.h"
 
+#define mod(x, y) (y + x) % y
+
 int shift_right(int ch, int offset) {
     int c = ch + offset;
     c -= islower(ch) ? 'a' : 'A';
@@ -9,10 +11,10 @@ int shift_right(int ch, int offset) {
     return c;
 }
 
-// int shift_left(int ch, int offset) {
-//     int c = ch + offset;
-//     c -= islower(ch) ? 'a' : 'A';
-//     c %= 26;
-//     c += islower(ch) ? 'a' : 'A';
-//     return c;
-// }
+int shift_left(int ch, int offset) {
+    int c = ch - offset;
+    c -= islower(ch) ? 'a' : 'A';
+    c = mod(c, 26);
+    c += islower(ch) ? 'a' : 'A';
+    return c;
+}
