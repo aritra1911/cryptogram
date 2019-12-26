81d05e931b5f9803b0e8aa3914531b9c5ff6e339
Aritra Sarkar <aritrasrkr1911@gmail.com>
2019-12-26 17:32:35 +0530

    [Morse] try fixing farnsworth speed

--- morse/morse_audio.c
+++ morse/morse_audio.c
@@ -1,3 +1,5 @@
+// Acknowledgement : https://morsecode.world/international/timing.html
+
 #include <stdio.h>
 #include "write_audio.h"
 
@@ -16,6 +18,9 @@ void init_audio(int wpm, int fwpm, int n, float f, float a) {
 }
 
 int write_code(int ch) {
+    // TODO, sue whoever puts 2 or more inter-word spaces.
+    // Or two inter-character spaces
+    // No seriously, fix this!
     switch (ch) {
         case '.':
             write_note(unit);
@@ -27,13 +32,32 @@ int write_code(int ch) {
             write_silence(unit);
             break;
 
-        case ' ':
-        case '/':
-            write_silence(2.0 * f_unit);
+        case ' ':  // Inter-character spacing
+            /* Here a difference is added as the last dit or dah already
+               wrote a unit of silence. Adding this difference makes that last
+               unit of silence an f_unit of silence and hence getting 3 total
+               f_units of silence.
+            */
+            write_silence(2.0 * f_unit + (f_unit - unit));
             break;
 
-        case '\n':
-            write_silence(6.0 * f_unit);
+        case '/':  // Inter-word spacing
+            /* Here I'm subtracting the difference. Now remember that this
+               requires a total of 7 f_units of silence. And it looks like
+               " / ". Firstly for the ' ', it adds 2*f_units + diff. But we
+               already got a unit of silence from the last dit or dah which
+               now combines with diff and gives a total 3*f_units of silence.
+               Next the '/' adds and makes the above 5*f_units - diff. Lastly,
+               the addition of another ' ' cancels the previously added diff and
+               returns correctly the required 7*f_units of silence.
+            */
+            write_silence(2.0 * f_unit - (f_unit - unit));
+            break;
+
+        case '\n':  // Inter-word spacing
+            // Same thing as previous, add the diff to make the last unit an
+            // f_unit.
+            write_silence(6.0 * f_unit + (f_unit - unit));
             break;
 
         default:
