66390ad790cd2d28bcd0575f90f0aca9ef6cf7eb
Aritra Sarkar <aritrasrkr1911@gmail.com>
2020-03-07 22:29:27 +0530

    [Morse] Fix Audio

--- morse/morse_audio.c
+++ morse/morse_audio.c
@@ -29,9 +29,6 @@ bool init_audio(int wpm, int fwpm, int n, float f, float a) {
 }
 
 int write_code(int ch) {
-    // TODO, sue whoever puts 2 or more inter-word spaces.
-    // Or two inter-character spaces
-    // No seriously, fix this!
     switch (ch) {
         case '.':
             write_note(unit);
@@ -44,9 +41,7 @@ int write_code(int ch) {
             break;
 
         case ' ':  // Inter-character spacing
-            if (previous_character == '.' ||
-                previous_character == '-' ||
-                previous_character == '/') {
+            if (previous_character == '.' || previous_character == '-') {
                 /* Here a difference is added as the last dit or dah already
                    wrote a unit of silence. Adding this difference makes that
                    last unit of silence an f_unit of silence and hence getting
@@ -59,19 +54,7 @@ int write_code(int ch) {
             write_silence(3.0 * f_unit);
             break;
 
-        case '/':  // Inter-word spacing
-            /* Here I'm subtracting the difference. Now remember that this
-               requires a total of 7 f_units of silence. And it looks like
-               " / ". Firstly for the ' ', it adds 2*f_units + diff. But we
-               already got a unit of silence from the last dit or dah which
-               now combines with diff and gives a total 3*f_units of silence.
-               Next the '/' adds and makes the above 5*f_units - diff. Lastly,
-               the addition of another ' ' cancels the previously added diff and
-               returns correctly the required 7*f_units of silence.
-            */
-            write_silence(2.0 * f_unit - (f_unit - unit));
-            break;
-
+        case  '/':
         case '\n':  // Inter-word spacing
             // Same thing as previous, add the diff to make the last unit an
             // f_unit.
