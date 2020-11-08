9921569d6cdeb39f711078b91976173031d10430
Aritra Sarkar <aritra1911@yahoo.com>
2020-11-08 15:58:50 +0530

    Generalize `mod` function

--- shifts/shifts.c
+++ shifts/shifts.c
@@ -2,7 +2,7 @@
 #include "shifts.h"
 
 int adjust(int, int);
-int mod26(int x);
+int mod_int(int, int);
 
 int shift_right(int ch, int offset) {
     int c = ch + offset;
@@ -16,13 +16,11 @@ int shift_left(int ch, int offset) {
 
 int adjust(int c, int ch) {
     c -= islower(ch) ? 'a' : 'A';
-    c = mod26(c);
+    c = mod_int(c, 26);
     c += islower(ch) ? 'a' : 'A';
     return c;
 }
 
-int mod26(int x) {
-    int y = x % 26;
-    if (y >= 0) return y;
-    return 26 + y;
+int mod_int(int x, int y) {
+    return ((x % y) + y) % y;
 }
